b19709ad2697f531d03a84c5dc28a7aca58bb210
gtongy <32930281+gtongy@users.noreply.github.com>
2019-10-20 19:12:26 +0900

    :recycle: split migrate files

--- postgres/migrate/1_create_tenants_and_users_tables.sql
+++ postgres/migrate/1_create_tenants_and_users_tables.sql
@@ -18,28 +18,3 @@ CREATE TABLE users
 ALTER TABLE users ENABLE ROW LEVEL SECURITY;
 -- usersテーブルに対して、ログインユーザーごとの取得行を絞り込み
 CREATE POLICY tenant_users_policy ON users USING(concat('tenant$', tenant_id) = current_user);
-
--- seed データの作成
-INSERT INTO tenants
-VALUES
-    (1, 'tenant1'),
-    (2, 'tenant2'),
-    (3, 'tenant3');
-INSERT INTO users
-VALUES
-    (1, 1, 'tenant1-user1'),
-    (2, 1, 'tenant1-user2'),
-    (3, 1, 'tenant1-user3'),
-    (4, 2, 'tenant2-user1'),
-    (5, 2, 'tenant2-user2'),
-    (6, 2, 'tenant2-user3');
-
--- tenantごとのログインユーザーを作成
-CREATE USER tenant$1 WITH LOGIN PASSWORD 'tenant$1';
-CREATE USER tenant$2 WITH LOGIN PASSWORD 'tenant$2';
-CREATE USER tenant$3 WITH LOGIN PASSWORD 'tenant$3';
-
--- 各ユーザーに全てのテーブルにGRANT ALL
-GRANT ALL ON ALL TABLES IN SCHEMA public TO tenant$1;
-GRANT ALL ON ALL TABLES IN SCHEMA public TO tenant$2;
-GRANT ALL ON ALL TABLES IN SCHEMA public TO tenant$3;
