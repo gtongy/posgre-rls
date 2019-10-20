73baab99a90fe5fe1d561caf792cfed137cebbe7
gtongy <32930281+gtongy@users.noreply.github.com>
2019-10-20 23:04:20 +0900

    :sparkles: modify golang sample modify

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,8 @@
 module github.com/gtongy/posgre-rls/api
 
 go 1.13
+
+require (
+	github.com/gocraft/dbr/v2 v2.6.3
+	github.com/lib/pq v1.2.0
+)
--- api/main.go
+++ api/main.go
@@ -1,7 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+
+	dbr "github.com/gocraft/dbr/v2"
+	_ "github.com/lib/pq"
+)
+
+var (
+	dbName = "rls"
+	host   = "postgres"
+	port   = "5432"
+)
+
+type User struct {
+	ID       int
+	TenantID int
+	Name     string
+}
 
 func main() {
-	fmt.Println("hello")
+	// 各ユーザー毎に別のコネクションを貼る
+	rootDB, err := rootDB()
+	defer rootDB.Close()
+	if err != nil {
+		log.Fatal("rootDB db connect failed!")
+	}
+	tenant1DB, err := tenant1DB()
+	defer tenant1DB.Close()
+	if err != nil {
+		log.Fatal("tenant1 db connect failed!")
+	}
+	tenant2DB, err := tenant2DB()
+	defer tenant2DB.Close()
+	if err != nil {
+		log.Fatal("tenant2 db connect failed!")
+	}
+	tenant3DB, err := tenant3DB()
+	defer tenant3DB.Close()
+	if err != nil {
+		log.Fatal("tenant3 db connect failed!")
+	}
+	ctx := context.Background()
+	var rootUsers, tenant1Users, tenant2Users, tenant3Users []User
+	// 各ユーザーごとにusersを検索
+	rootDB.NewSession(nil).Select("*").From("users").LoadContext(ctx, &rootUsers)
+	tenant1DB.NewSession(nil).Select("*").From("users").LoadContext(ctx, &tenant1Users)
+	tenant2DB.NewSession(nil).Select("*").From("users").LoadContext(ctx, &tenant2Users)
+	tenant3DB.NewSession(nil).Select("*").From("users").LoadContext(ctx, &tenant3Users)
+
+	// 結果は以下のようになる。
+	// [{1 1 tenant1-user1} {2 1 tenant1-user2} {3 1 tenant1-user3} {4 2 tenant2-user1} {5 2 tenant2-user2} {6 2 tenant2-user3}]
+	log.Println(rootUsers)
+	// [{1 1 tenant1-user1} {2 1 tenant1-user2} {3 1 tenant1-user3}]
+	log.Println(tenant1Users)
+	// [{4 2 tenant2-user1} {5 2 tenant2-user2} {6 2 tenant2-user3}]
+	log.Println(tenant2Users)
+	// []
+	log.Println(tenant3Users)
+}
+
+func rootDB() (*dbr.Connection, error) {
+	return dbr.Open("postgres", "host="+host+" port="+port+" user=dev password=dev dbname="+dbName+" sslmode=disable", nil)
+}
+
+func tenant1DB() (*dbr.Connection, error) {
+	return dbr.Open("postgres", "host="+host+" port="+port+" user=tenant$1 password=tenant$1 dbname="+dbName+" sslmode=disable", nil)
+}
+
+func tenant2DB() (*dbr.Connection, error) {
+	return dbr.Open("postgres", "host="+host+" port="+port+" user=tenant$2 password=tenant$2 dbname="+dbName+" sslmode=disable", nil)
+}
+
+func tenant3DB() (*dbr.Connection, error) {
+	return dbr.Open("postgres", "host="+host+" port="+port+" user=tenant$3 password=tenant$3 dbname="+dbName+" sslmode=disable", nil)
 }
